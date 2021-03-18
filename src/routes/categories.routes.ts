735956ed7e69dba54cd02b50b6f8bccd485c8b71
guiialmeiida <gui.almeidacrz@gmail.com>
2021-03-18 08:30:35 -0300

    Criando service de especificações e separando em módulos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategorySerce } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategorySerce } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -22,3 +22,4 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 export { categoriesRoutes };
+
