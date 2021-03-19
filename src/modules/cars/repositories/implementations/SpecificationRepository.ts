cd0725ddcf1c2958e2ce8c23c25ddab94d585e41
guiialmeiida <gui.almeidacrz@gmail.com>
2021-03-19 08:26:16 -0300

    Conhecendo Singleton Pattern e separando repositórios

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,5 +1,5 @@
-import { Specification } from "../model/Specification";
-import { ICreateSpecificationDTO, ISpecificationRepository } from "./ISpecificationsRepository";
+import { Specification } from "../../model/Specification";
+import { ICreateSpecificationDTO, ISpecificationRepository } from "../ISpecificationsRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
     private specifications: Specification[];
