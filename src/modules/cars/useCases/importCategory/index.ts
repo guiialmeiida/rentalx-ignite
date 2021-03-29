ba91ca0ebf91c07633fff6757453bbe765bec982
guiialmeiida <gui.almeidacrz@gmail.com>
2021-03-29 08:59:09 -0300

    Refatorando caso de uso categorias

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,7 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const caterogiesRepository = CategoriesRepository.getInstance();
+const caterogiesRepository = null;
 const importCategoryUseCase = new ImportCategoryUseCase(caterogiesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
