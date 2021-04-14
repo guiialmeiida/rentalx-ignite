a6cda350aa5e47fbade3707576346f08ecf92f09
guiialmeiida <gui.almeidacrz@gmail.com>
2021-04-14 08:24:23 -0300

    Continuando o caso de uso de carros

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -12,7 +12,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
         daily_rate,
         description,
         name
-    }: ICreateCarDTO): Promise<void> {
+    }: ICreateCarDTO): Promise<Car> {
         const car = new Car();
 
         Object.assign(car, {
@@ -26,8 +26,13 @@ class CarsRepositoryInMemory implements ICarsRepository {
         });
 
         this.cars.push(car);
+
+        return car;
     }
 
+    async findByLicensePlate(license_plate: string): Promise<Car> {
+        return this.cars.find((car) => car.license_plate === license_plate);
+    }
 }
 
 export { CarsRepositoryInMemory };
--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,5 +1,6 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/in-memory/ICarsRepository";
-import { inject, injectable } from "tsyringe";
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
     name: string,
@@ -11,10 +12,10 @@ interface IRequest {
     category_id: string;
 }
 
-@injectable()
+//@injectable()
 class CreateCarUseCase {
     constructor(
-        @inject("CarsRepository")
+        //@inject("CarsRepository")
         private carsRepository: ICarsRepository
     ) { }
 
@@ -26,8 +27,15 @@ class CreateCarUseCase {
         fine_amount,
         brand,
         category_id
-    }: IRequest): Promise<void> {
-        this.carsRepository.create({
+    }: IRequest): Promise<Car> {
+
+        const carAlreadyExists = await this.carsRepository.findByLicensePlate(license_plate);
+
+        if (carAlreadyExists) {
+            throw new AppError("Car already exists");
+        }
+
+        const car = await this.carsRepository.create({
             name,
             description,
             daily_rate,
@@ -36,6 +44,8 @@ class CreateCarUseCase {
             brand,
             category_id
         });
+
+        return car;
     }
 }
 
