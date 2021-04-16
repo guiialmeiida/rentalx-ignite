6df0d208ad16861608f6ad5e0a6e9028c7add9c2
guiialmeiida <gui.almeidacrz@gmail.com>
2021-04-16 07:33:35 -0300

    Estruturando a entidade de carros

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/in-memory/ICarsRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { container } from "tsyringe";
 
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
 );
