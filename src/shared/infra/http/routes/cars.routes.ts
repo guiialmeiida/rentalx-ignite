351d0355e01efd6b2c5b811535a84d50e67334c0
guiialmeiida <gui.almeidacrz@gmail.com>
2021-04-20 07:47:03 -0300

    Continuando listagem de carros

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 import { Router } from "express";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -6,12 +7,18 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListCategoriesController();
 
 carsRoutes.post(
     "/",
     ensureAuthenticated,
     ensureAdmin,
     createCarController.handle
+);
+
+carsRoutes.get(
+    "/available",
+    listAvailableCarsController.handle
 );
 
 export { carsRoutes };
