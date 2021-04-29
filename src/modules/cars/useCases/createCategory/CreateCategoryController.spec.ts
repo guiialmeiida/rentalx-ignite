11884e969688f4e0abf23ea9c17a252a50db1b12
guiialmeiida <gui.almeidacrz@gmail.com>
2021-04-29 07:55:02 -0300

    Configurando supertest e criando primeiro teste de integração

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,14 @@
+import { app } from "@shared/infra/http/app";
+import request from "supertest";
+
+describe("Create Category Controller", async () => {
+
+    it("Should be able to create a new category", async () => {
+        const response = await request(app).post("/categories").send({
+            name: "Category Supertest",
+            description: "Category Supertest",
+        });
+
+        expect(response.status).toBe(201);
+    });
+});
