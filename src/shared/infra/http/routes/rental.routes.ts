656c3852beedacc95ad8a3995c222dd9f896e55a
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-04 07:44:47 -0300

    Listagem de aluguéis do usuário

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevoluitonRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -7,6 +8,7 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController()
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 
@@ -14,6 +16,12 @@ rentalRoutes.post(
     "/devolution/:id",
     ensureAuthenticated,
     devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+    "/user",
+    ensureAuthenticated,
+    listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
