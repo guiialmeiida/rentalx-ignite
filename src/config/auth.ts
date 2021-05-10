88705861de11da2fa2508dda1b8f531658af433b
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-10 08:43:02 -0300

    Refatorando autenticação do usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "0e7e3d877412a8d2c058b6f27b1a38ea",
+    expires_in_token: "15m",
+    secret_refresh_token: "61704e176edca7b55d44106775696fa1",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+}
