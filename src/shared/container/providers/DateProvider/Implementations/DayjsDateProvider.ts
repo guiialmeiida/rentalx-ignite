3b6d8db9d1ba6d3002320bd23d5a0c661d4726d4
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-18 08:20:54 -0300

    Caso de uso de reset de senha

--- src/shared/container/providers/DateProvider/Implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/Implementations/DayjsDateProvider.ts
@@ -5,6 +5,7 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
+
     compareInHours(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date);
         const start_date_utc = this.convertToUTC(start_date);
@@ -33,6 +34,10 @@ class DayjsDateProvider implements IDateProvider {
 
     addHours(hours: number): Date {
         return dayjs().add(hours, "hour").toDate();
+    }
+
+    compareIfBefore(start_date: Date, end_date: Date): boolean {
+        return dayjs(start_date).isBefore(end_date);
     }
 }
 
