93c102ce79bdad4d1e40adc959d9acc5b4c72157
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-18 07:53:38 -0300

    Inserindo template engine para envio de e-mail

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,11 @@
 interface IMailProvider {
-    sendMail(to: string, subject: string, body: string): Promise<void>;
+    sendMail(
+        to: string,
+        subject: string,
+        variables: any,
+        path: string
+    ): Promise<void>;
 }
 
 export { IMailProvider };
+
